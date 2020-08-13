1ebbea5f8f29a2c02e5a90cdb10cb498292a3b79
Ramsey Njire <ramseynjire@gmail.com>
2020-08-13 19:02:05 +0300

    Write tests for #my_each and #my_each_with_index methods

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -3,12 +3,34 @@ require "../main"
 
 describe Enumerable do
   let (:my_array) {[1, 2, 3, 4]}
+  # let(:test_case) {}
   describe '#my_each' do
+    #negative test
     context "No block given" do
       it "should return an enumerable" do
         expect(my_array.my_each).to be_a(Enumerable)
       end
     end
+    #positive test
+    context "block given" do
+      it "should match result for original #each method" do
+        expect(my_array.my_each{|e| puts e}).to eq(my_array.each{|e| puts e})
+      end
+    end
   end
+  describe '#my_each_with_index' do
+  #negative test
+  context "No block given" do
+    it "should return an enumerable" do
+      expect(my_array.my_each_with_index).to be_a(Enumerable)
+    end
+  end
+  #positive test
+  context "block given" do
+    it "should match result for original #each_with_index method" do
+      expect(my_array.my_each_with_index{|e, i| puts e * i}).to eq(my_array.each_with_index{|e, i| puts e * i})
+    end
+  end
+end
 end
 
